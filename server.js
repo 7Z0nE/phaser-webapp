7da9f3fbed69d95fea3144905c009d5da45e5c1c
Frederik Wegner <onlyknoppas@gmail.com>
2017-12-28 00:35:17 +0100

    Adapted front end to back and. File upload works.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fileUpload = require('fileUpload');
+const fileUpload = require('express-fileupload');
 const TokenGenerator = require('uuid-token-generator');
 const fs = require('fs');
 
@@ -22,34 +22,36 @@ app.get('/status/:token', (req, res) => {
 
     if(fs.existsSync(processorOutPath + token)) {
         //TODO: create actual link.
-        res.status(201).send('Link');
+        return res.status(201).send('Link');
     }
     else {
         if(fs.existsSync(processorInputPath + token)) {
-            res.status(200).send('queue');
+            return res.status(200).send('queue');
         }
         else {
-            res.status(404).send('No task with token ' + token + ' found');
+            return res.status(404).send('No task with token ' + token + ' found');
         }
     }
 });
 
 /**
- * Receives a sound file from the client. The file should be named 'input'.
+ * Receives a sound file from the client. The file should be named 'file'.
  * See https://www.npmjs.com/package/express-fileupload for more information.
  * Responds with a token for the task in queue.
  */
 app.post('/upload', (req, res) => {
-    if(!req.files.input || !req.files) {
+    if(!req.files.file || !req.files) {
         return res.status(400).send("No files in request.");
     }
 
-    let inputFile = req.files.input;
+    let inputFile = req.files.file;
 
     let token = tokenGenerator.generate();
-    inputFile.mv('/processor/input/'+token);
-
-    req.status(202).send(token);
+    inputFile.mv('./processor/input/' + token, (err) => {
+        if (err)
+            return res.status(500).send(err);
+        return res.status(202).send(token);
+    });
 });
 
-app.listen(3000, () => console.log("Running on port 3000"));
+app.listen(8000, () => console.log("Running on port 8000"));
