f099c23fb0d240394d830b16c4f8ac13da5fe051
Frederik Wegner <onlyknoppas@gmail.com>
2017-12-28 12:30:54 +0100

    server now properly enqueues a task

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fileUpload = require('express-fileupload');
 const TokenGenerator = require('uuid-token-generator');
 const spawn = require('child_process').spawn;
 const fs = require('fs');
+const util = require('util');
 
 const processorPath = '/processor/';
 const processorExec = processorPath + 'processor.sh';
@@ -46,18 +47,52 @@ app.post('/upload', (req, res) => {
     if(!req.files.file || !req.files) {
         return res.status(400).send("No files in request.");
     }
+    
+    console.log("upload: " + JSON.stringify(req.body));
 
     let inputFile = req.files.file;
 
     let token = tokenGenerator.generate();
-    inputFile.mv(processorQueuePath + token, (err) => {
-        if (err)
+
+    let dir = fs.mkdirSync(token, (err) => {
+        if(err) {
             return res.status(500).send(err);
+        }
+    });
+
+    let config = 'wav\n'
+        + req.body.bpm + '\n'
+        + req.body.length + '\n'
+        + req.body.repetitions + '\n'
+        + req.body.factor + '\n';
+
+    fs.writeFileSync(token+'/config', config, (err) => {
+        console.log('config: '+config);
+        if(err) {
+            fs.rmdirSync('/'+token);
+            return res.status(500).send(err);
+        }
+    });
+
+    inputFile.mv(token+'/data.wav', (err) => {
+        if (err) {
+            fs.rmdirSync(token);
+            return res.status(500).send(err);
+        }
         return res.status(202).send(token);
+    });
+
+    fs.renameSync(token, processorQueuePath+token, (err) => {
+        if(err) {
+            fs.rmdirSync(token);
+            return res.status(500).send(err);
+        }
     });
 });
 
-console.log("Starting processor with " + 'bash ' + processorExec + " in " + __dirname + processorPath)
+console.log("Starting processor with " + 'bash ' 
+    + processorExec + " in "
+    + __dirname + processorPath);
 const processor = spawn('bash',
     args=[__dirname + processorExec, __dirname + processorPath],
     { 
@@ -66,7 +101,7 @@ const processor = spawn('bash',
     );
 
 processor.stdout.on('data', function (data) {
-    console.log('processor out: ' + data);
+    //console.log('processor out: ' + data);
 });
   
 processor.stderr.on('data', function (data) {
