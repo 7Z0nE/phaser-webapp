569281b5af04d53199c17c1ef662a48ffe4ecd3d
Gol0l <Golol@gmx.de>
2017-12-28 00:19:48 +0000

    wadawga

--- processor/processor.sh
+++ processor/processor.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 path=$1
 while true; do
-	if [ -z "$(ls -A "$path/queue")" ]; then
+	if [ -z "$(ls -A "queue")" ]; then
 		echo "affe"
 	else
-		currentToken=$(ls -t $path/queue | head -1)
-		currentConfig=$path/queue/$(currentToken)/config
+		currentToken=$(ls -t queue | head -1)
+		currentConfig=queue/$(currentToken)/config
 		currentData=data.wav
-		./scripts/phase $path/queue/$currentToken/$currentData $path/finished/$currentToken$currentData 120 4 32 0.8
-		rm -r -f $path/queue/$currentToken
+		./scripts/phase queue/$currentToken/$currentData finished/$currentToken$currentData 120 4 32 0.8
+		rm -r -f $pathqueue/$currentToken
 	fi
 done
