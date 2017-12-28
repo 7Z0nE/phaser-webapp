3c82f90c66061703dc090e7517a1580053638d92
Frederik Wegner <onlyknoppas@gmail.com>
2017-12-28 11:15:33 +0100

    asdf

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const TokenGenerator = require('uuid-token-generator');
+const spawn = require('child_process').spawn;
 const fs = require('fs');
+
+const processorPath = '/processor/';
+const processorExec = processorPath + 'processor.sh';
+const processorQueuePath = 'processor/queue/';
+const processorOutPath = 'processor/finished/';
 
 const app = express();
 const tokenGenerator = new TokenGenerator();
@@ -9,9 +15,6 @@ const tokenGenerator = new TokenGenerator();
 app.use(express.static('public'));
 
 app.use(fileUpload());
-
-const processorInputPath = '/processor/input/';
-const processorOutPath = '/processor/output/';
 
 /**
  * Get the status for a task token as string. If it is finished,
@@ -25,7 +28,7 @@ app.get('/status/:token', (req, res) => {
         return res.status(201).send('Link');
     }
     else {
-        if(fs.existsSync(processorInputPath + token)) {
+        if(fs.existsSync(processorQueuePath + token)) {
             return res.status(200).send('queue');
         }
         else {
@@ -47,11 +50,31 @@ app.post('/upload', (req, res) => {
     let inputFile = req.files.file;
 
     let token = tokenGenerator.generate();
-    inputFile.mv('./processor/input/' + token, (err) => {
+    inputFile.mv(processorQueuePath + token, (err) => {
         if (err)
             return res.status(500).send(err);
         return res.status(202).send(token);
     });
 });
 
-app.listen(8000, () => console.log("Running on port 8000"));
+console.log("Starting processor with " + 'bash ' + processorExec + " in " + __dirname + processorPath)
+const processor = spawn('bash',
+    args=[__dirname + processorExec, __dirname + processorPath],
+    { 
+        cwd: __dirname + processorPath
+    }
+    );
+
+processor.stdout.on('data', function (data) {
+    console.log('processor out: ' + data);
+});
+  
+processor.stderr.on('data', function (data) {
+    console.log('processor err: ' + data);
+});
+
+processor.on('exit', function (code) {
+    console.log('processor exited with code ' + code);
+});
+
+app.listen(8000, () => console.log("Running on port 8000, in " + __dirname));
