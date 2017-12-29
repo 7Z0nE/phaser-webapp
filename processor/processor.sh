f637dbd5b890cde6fe02ff9ac8178983f4933a9e
Gol0l <Golol@gmx.de>
2017-12-29 22:01:54 +0000

    removed echos and change output format in processor

--- processor/processor.sh
+++ processor/processor.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 while true; do
 	if [ -z "$(ls -A "queue")" ]; then
-		echo "affe"
+		true
 	else
 		currentToken=$(ls -t queue | head -1)
-		echo $currentToken
 		currentConfig=queue/$currentToken/config
-		
+
 		type=$(sed -n '1p' < $currentConfig)
 		bpm=$(sed -n '2p' < $currentConfig)
 		length=$(sed -n '3p' < $currentConfig)
@@ -14,7 +13,8 @@ while true; do
 		factor=$(sed -n '5p' < $currentConfig)
 
 		currentData=data.$type
-		./scripts/phase queue/$currentToken/$currentData finished/$currentToken$currentData $bpm $length $repetitions $factor
+		mkdir ./finished/$currentToken
+		./scripts/phase queue/$currentToken/$currentData finished/$currentToken/$currentData $bpm $length $repetitions $factor
 		rm -r -f queue/$currentToken
 	fi
 done
